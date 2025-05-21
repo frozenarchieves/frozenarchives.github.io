8c8d50ec79c34f084d0fe6ce7d26ff91e312bec8
frozenarchives <tirpudepranaykumar@gmail.com>
2025-05-21 16:50:00 +0000

    Deploying to gh-pages from @ frozenarchives/frozenarchives.github.io@218e882e3cad8e701c66f3aa714b49c985260bd7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,15 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
-            },},{
+            },},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
